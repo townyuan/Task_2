4a096f9ee2782c57d6a8a742ea44c16797a18a12
yuan <townyuan@gmail.com>
2023-05-25 16:57:11 +0800

    調整WRD
    調整WRD

--- js/all.js
+++ js/all.js
@@ -1,10 +1,27 @@
 
-//常見問題
+
 $(document).ready(function() {
+
+  //選單開合
+  $(".menu-switch").click(function (e) {
+		e.preventDefault();
+		$(".menu-switch").toggleClass("active");
+		$(".jq-dropdown").slideToggle();
+	});
+  
+
+
+  //常見問題
   $('.common-problem .accordion-header').click(function() {
     let content = $(this).next('.accordion-content');
     content.slideToggle();
     let icon = $(this).find('.accordion-icon');
     icon.toggleClass('rotate');
+  });
+
+  // gotop
+  $('.back-to-top').click(function(){
+    // 使用動畫效果返回頂部
+    $('html, body').animate({scrollTop: 0}, 500);
   });
 });
